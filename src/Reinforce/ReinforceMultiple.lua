02d4d72a65b02e1d170e303fe11e77246b887729
nosyndicate <nosyndicate@gmail.com>
2015-12-16 16:37:13 -0500

    add reinforce multinomial case

--- src/Reinforce/ReinforceMultiple.lua
+++ src/Reinforce/ReinforceMultiple.lua
@@ -8,10 +8,10 @@
 local ReinforceMultiple, parent = torch.class("ReinforceMultiple","nn.Module");
 
 --[[
-	g(y,p_1,p_2,p_3,...,p_n) - multinomial distribution function 
+	g(y,p_1,p_2,p_3,...,p_n) - Multinomial distribution function 
 	y      - Sampled values (1 to n) (self.output will encoding the sampled value in one-hot format)
 	p_i    - Probability of sampling a value of i
-
+	g(y,p_1,p_2,p_3,...,p_n) = p_i   (if y = i)
 	       
 	Input is the vector of p_i (p_1, p_2, p_3, ..., p_n)
 	Output is the sampled value from the Multinomial distribution in one-hot encoding
@@ -19,7 +19,12 @@ local ReinforceMultiple, parent = torch.class("ReinforceMultiple","nn.Module");
 function ReinforceBinary:updateOutput(input)
 	self.output.resizeAs(input);
 	self.sampleValue = self.sampleValue or input.new();
-
+	self.sampleValue:resizeAs(input):copy(input):add()
+	
+	
+	-- create one hot encoding
+	self.output:zero();
+	
 
 
 	return self.output;
@@ -27,24 +32,24 @@ end
 
 --[[
 	Ignore the gradOutput since we are not measure the derivative of the loss w.r.t p
-	Instead, we measure the derivative of log Bernoulli w.r.t p
-	d ln(g(y,p))       1         d g(y,p)
-	------------ =  -------  *  -----------
-	    d p          g(y,p)         d p
+	Instead, we measure the derivative of log Multinomial w.r.t p
+	p - probability vector (p_1, p_2, p_3, ..., p_n)
+	  d ln(g(y,p))           1            d g(y,p)
+	---------------- =  -----------  *  ------------
+	      d p              g(y,p)           d p
 	    
-	=    1/p * 1          (if y = 1)
-	=    1/(1-p) * (-1)   (if y = 0)
-	=    (y-p) / p(1-p)   (general form)
+	=    1/p_i * 1          (if y = i)
+	=    0                  (if y = 0)
 --]]
 function ReinforceBinary:updateGradInput(input, gradOutput)
-	self.gradInput:resizeAs(input);
-	-- compute (y-p)
-	self.gradInput:copy(self.output):add(-1, input);
-	-- compute p(1-p)
+	self.gradInput:resizeAs(input):zero();
+	self.gradInput:copy(self.output);
 	self.denominator = self.denominator or input.new();
-	self.denominator:resizeAs(input);
-	self.denominator:fill(1).add(-1, input):cmul(input);
+	
+	-- add some tiny value in case some probability is 0
+	self.denominator:resizeAs(input):copy(input):add(0.00000001);
 	self.gradInput:cdiv(self.denominator);
+
 	
 	-- multiply by reward, so the rest of the layer doesn't need to worry about the reward
 	self.gradInput:cmul(self:rewardAs(input)); 
